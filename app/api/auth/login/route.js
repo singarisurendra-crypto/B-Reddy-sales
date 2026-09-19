8cbd2c1150b2fca21960da9486f64d4d1089c3a3
singarisurendra-crypto <singarisurendra@gmail.com>
2026-09-19 12:25:21 +0530

    Add files via upload

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import { supabaseAdmin } from '../../../../lib/server/supabaseAdmin';
-import { createSession } from '../../../../lib/server/session';
+import { supabaseAdmin } from '../../server/supabaseAdmin';
+import { createSession } from '../../server/session';
 
 export async function POST(request) {
   try {
